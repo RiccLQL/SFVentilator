757abf9f648ceac244ba2b3a0464115a964c09ca
Ian Langleben <ilangleben19@gmail.com>
2020-03-27 12:09:52 -0400

    Testing on RPi

--- react-ui/src/SettingButton.jsx
+++ react-ui/src/SettingButton.jsx
@@ -23,7 +23,7 @@ const sliderLabelSize = 40;
 
 const useStyles = makeStyles(theme => ({
     root: {
-        margin: theme.spacing(1),
+        margin: theme.spacing(0, 1),
     },
     button: {
         backgroundColor: theme.palette.primary.main,
